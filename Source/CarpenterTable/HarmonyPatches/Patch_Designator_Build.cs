6993e279351a18a0291838ea79d7a0cd5f6585e3
XeoNovaDan <toghill7252010@hotmail.co.uk>
2019-07-17 20:41:22 +0100

    Designator godmode hotfix

--- Source/CarpenterTable/HarmonyPatches/Patch_Designator_Build.cs
+++ Source/CarpenterTable/HarmonyPatches/Patch_Designator_Build.cs
@@ -20,8 +20,8 @@ namespace CarpenterTable
 
             public static void Postfix(Designator_Build __instance, ref bool __result)
             {
-                // If the PlacingDef is a piece of furniture and the 'restrict furniture to carpenter tables' setting is enabled, return false
-                if (CarpenterTablesSettings.restrictFurnitureConstruction && __instance.PlacingDef.IsFurniture())
+                // If the 'restrict furniture construction' setting is enabled, god mode is not enabled and the PlacingDef is furniture, hide the designator
+                if (CarpenterTablesSettings.restrictFurnitureConstruction && !DebugSettings.godMode && __instance.PlacingDef.IsFurniture())
                     __result = false;
             }
 
